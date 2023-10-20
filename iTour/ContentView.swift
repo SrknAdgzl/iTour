46fe424438c14ff2cffbd06a91a77ea067571456
SrknAdgzl <138037543+SrknAdgzl@users.noreply.github.com>
2023-10-20 19:30:39 +0300

    Adding and editing specs are added.

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -5,17 +5,49 @@
 //  Created by Serkan ADIGÜZEL on 20.10.2023.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+    @State private var path = [Destination]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            List {
+                ForEach(destinations) { destination in
+                    NavigationLink(value: destination) {
+                        VStack(alignment: .leading) {
+                            Text(destination.name)
+                                .font(.headline)
+                            
+                            Text(destination.date.formatted(date: .long, time: .shortened))
+                        }
+                    }
+                }
+                .onDelete(perform: deleteDestinations)
+            }
+            .navigationTitle("iTour")
+            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+            .toolbar {
+                Button("Add Destination", systemImage: "plus", action: addDestination)
+            }
         }
-        .padding()
+    }
+    
+    func addDestination() {
+        let destination = Destination()
+        modelContext.insert(destination)
+        path = [destination]
+    }
+    
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
     }
 }
 
---
+++ iTour/EditDestinationView.swift
@@ -0,0 +1,46 @@
+//
+//  EditDestinationView.swift
+//  iTour
+//
+//  Created by Serkan ADIGÜZEL on 20.10.2023.
+//
+
+import SwiftData
+import SwiftUI
+
+struct EditDestinationView: View {
+    
+    @Bindable var destination: Destination
+    
+    var body: some View {
+        Form {
+            TextField("Name", text: $destination.name)
+            TextField("Details", text: $destination.details, axis: .vertical)
+            DatePicker("Date", selection: $destination.date)
+
+            Section("Priority") {
+                Picker("Priority", selection: $destination.priority) {
+                    Text("Meh").tag(1)
+                    Text("Maybe").tag(2)
+                    Text("Must").tag(3)
+                }
+                .pickerStyle(.segmented)
+            }
+        }
+        .navigationTitle("Edit Destination")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Destination.self, configurations: config)
+
+        let example = Destination(name: "Example Destination", details: "Example details go here and will automatically expand vertically as they are edited.")
+        return EditDestinationView(destination: example)
+            .modelContainer(container)
+    } catch {
+        fatalError("Failed to create model container.")
+    }
+}
