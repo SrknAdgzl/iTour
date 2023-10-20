5567b3bb3d4a72b526e58ca4d56001764ae43dfa
SrknAdgzl <138037543+SrknAdgzl@users.noreply.github.com>
2023-10-20 18:36:50 +0300

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Serkan ADIGÜZEL on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
